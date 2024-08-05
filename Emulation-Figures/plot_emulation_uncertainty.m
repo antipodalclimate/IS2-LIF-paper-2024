0913e0c3638c6e466c06c9af5a11d6ca7e7b9b0b
Christopher Horvat <christopher_horvat@brown.edu>
2024-08-05 13:39:44 -0400

    restructuring plotting and driver

--- Emulation-Figures/plot_emulation_uncertainty.m
+++ Emulation-Figures/plot_emulation_uncertainty.m
@@ -1,4 +1,4 @@
-
+function plot_emulation_uncertainty(Figure_folder,true_SIC,length_ice_measured,length_measured)
 
 usable = true_SIC < .99 & true_SIC > 0.1 & ~isnan(true_SIC);
 
@@ -7,16 +7,19 @@ usable = true_SIC < .99 & true_SIC > 0.1 & ~isnan(true_SIC);
 % permutations of those crossings
 n_images = sum(usable);
 n_crossings = size(length_measured,2);
-perm_length = 50; 
+
+% How long of a permutation
+perm_length = 100;
+% Number of permutations we choose
 n_perms = 4*n_crossings;
 
 
 % Measured SIC when accumulating crossings
 im_meas_SIC = nan(n_images,perm_length,n_perms);
-tot_length = im_meas_SIC; 
+tot_length = im_meas_SIC;
 
 % For each permutation, take the cumulative sum in each image of the random
-% permutation of images. 
+% permutation of images.
 for j = 1:n_perms
 
     % randomly permute the number of crossings
@@ -29,83 +32,99 @@ end
 
 %% Compute bias fields
 
-% Difference between actual SIC and the accumulated SIC. 
+% Difference between actual SIC and the accumulated SIC.
 SIC_bias = bsxfun(@minus,true_SIC(usable),im_meas_SIC);
 
 % Long-term LIF
 LIF_star = mean(im_meas_SIC(:,end,:),3);
 % Bias from true value
-Bias_i = true_SIC(usable) - LIF_star; 
+Bias_i = true_SIC(usable) - LIF_star;
 % Variability at end of crossings
-Std_i = 100*std(SIC_bias(:,end,:),[],3,'omitnan'); 
+Std_i = 100*std(SIC_bias(:,end,:),[],3,'omitnan');
 
 % Take the mean bias as a function of crossing number.
 
 % Mean bias per crossing averaged over all permutations and images
-Bias_n = 100*squeeze(mean(SIC_bias,[1 3],"omitnan")); 
+Bias_n = 100*squeeze(mean(SIC_bias,[1 3],"omitnan"));
 
 % Mean absolute bias per crossing, across all permutations and images
-Bias_abs_n = 100*squeeze(mean(mean(abs(SIC_bias),3),1,"omitnan")); 
+Bias_abs_n = 100*squeeze(mean(mean(abs(SIC_bias),3),1,"omitnan"));
 
-Std_n = 100*squeeze(mean(std(SIC_bias,[],3,'omitnan'),1,'omitnan')); 
+Std_n = 100*squeeze(mean(std(SIC_bias,[],3,'omitnan'),1,'omitnan'));
 
 %%
 
 % Mean absolute bias per crossing, when we average over all permutations
 % first, then take the absolute value and average across all images
-Bias_abs_barfirst_angle = 100*squeeze(mean(abs(mean(SIC_bias,3,'omitnan')),'omitnan')); 
+Bias_abs_barfirst_angle = 100*squeeze(mean(abs(mean(SIC_bias,3,'omitnan')),'omitnan'));
 
 % Mean absolute bias per crossing, when we average over all images first
 % first, then take the absolute value and average over all permutations.
 % I think this one should basically be zero since the mean bias per image
-% is pretty low. 
-Bias_abs_bar_anglefirst = 100*squeeze(mean(abs(mean(SIC_bias,1,'omitnan')),'omitnan')); 
+% is pretty low.
+Bias_abs_bar_anglefirst = 100*squeeze(mean(abs(mean(SIC_bias,1,'omitnan')),'omitnan'));
 
 
 %% Standard deviations
 
 % Take the standard deviation of the average bias after averaging out all
 % permutations
-Bias_std_angle = 100*squeeze(std(mean(SIC_bias,3),[],1,"omitnan")); 
+Bias_std_angle = 100*squeeze(std(mean(SIC_bias,3),[],1,"omitnan"));
 % Take the standard deviation of all including all permutations. This will
-% decay faster because all individual images have a decaying part. 
-Bias_std_all = 100*squeeze(std(SIC_bias,[],[1 3],'omitnan')); 
+% decay faster because all individual images have a decaying part.
+Bias_std_all = 100*squeeze(std(SIC_bias,[],[1 3],'omitnan'));
 
-Bias_abs_595_all = 100*squeeze(prctile(abs(SIC_bias),100*[exp(-2) 1 - exp(-2)],[1 3])); 
-Bias_abs_595_barfirst = 100*squeeze(prctile(abs(mean(SIC_bias,3)),100*[exp(-2) 1 - exp(-2)],[1])); 
+Bias_abs_595_all = 100*squeeze(prctile(abs(SIC_bias),100*[exp(-2) 1 - exp(-2)],[1 3]));
+Bias_abs_595_barfirst = 100*squeeze(prctile(abs(mean(SIC_bias,3)),100*[exp(-2) 1 - exp(-2)],[1]));
+
+%%
 
 figure(1)
 clf
 
-subplot(321)
-histogram(Bias_i)
-
-subplot(322)
-histogram(Std_i); 
+subplot(421)
+histogram(100*Bias_i,[-10:.1:10],'Normalization','probability','facecolor',[.8 .4 .4],'edgecolor','none');
+grid on; box on;
+xlabel('B_i'); ylabel('PDF');
+title('Best-case bias $B_i = LIF_i^* - SIC$','interpreter','latex')
+xline(prctile(100*Bias_i,75),'k','LineWidth',1)
+xline(prctile(100*Bias_i,25),'k','LineWidth',1)
+set(gca,'yticklabel','')
+xlim([-5 5])
+subplot(422)
+histogram(Std_i,[0:.05:5],'Normalization','probability','facecolor',[.8 .4 .4],'edgecolor','none');
+grid on; box on;
+xlabel('B_i'); ylabel('PDF');
+title('Path-based variance in $B_i$','interpreter','latex')
+xline(prctile(Std_i,75),'k','LineWidth',1)
+xline(prctile(Std_i,25),'k','LineWidth',1)
+set(gca,'yticklabel','')
+xlim([0 3])
 
 %%
 
 
-subplot(312)
+subplot(412)
 % Plot the mean over all images and permutations as a function of crossing
 % number
-plot(1:perm_length,Bias_n,'k','linewidth',1); 
+plot(1:perm_length,Bias_n,'k','linewidth',1);
 hold on
-jbfill(1:perm_length,Bias_n + Std_n,Bias_n - Std_n,[.4 .4 .4],[0 0 0],1,0.25); 
-grid on; box on; 
+jbfill(1:perm_length,Bias_n + Std_n,Bias_n - Std_n,[.4 .4 .4],[0 0 0],1,0.25);
+jbfill(1:perm_length,Bias_n + Bias_std_angle,Bias_n - Bias_std_angle,[.4 .4 .8],[0 0 0],1,0.25);
+grid on; box on;
 xlim([1 perm_length])
 title('Mean Biases and Confidence Intervals','interpreter','latex')
 
-subplot(313)
+subplot(413)
 % Now looking at absolute biases
-plot(1:perm_length,Bias_abs_n,'k','linewidth',1); 
+plot(1:perm_length,Bias_abs_n,'k','linewidth',1);
 hold on
 
-jbfill(1:perm_length,Bias_abs_595_all(1,:),Bias_abs_595_all(2,:),[.4 .4 .4],[0 0 0],1,0.25); 
+jbfill(1:perm_length,Bias_abs_595_all(1,:),Bias_abs_595_all(2,:),[.4 .4 .4],[0 0 0],1,0.25);
 hold on
-plot(1:perm_length,Bias_abs_barfirst_angle,'b','linewidth',1); 
-jbfill(1:perm_length,Bias_abs_595_barfirst(1,:),Bias_abs_595_barfirst(2,:),[.4 .4 .8],[0 0 0],1,0.25); 
-grid on; box on; 
+plot(1:perm_length,Bias_abs_barfirst_angle,'b','linewidth',1);
+jbfill(1:perm_length,Bias_abs_595_barfirst(1,:),Bias_abs_595_barfirst(2,:),[.4 .4 .8],[0 0 0],1,0.25);
+grid on; box on;
 xlim([1 perm_length])
 
 
@@ -118,34 +137,34 @@ title('Absolute Biases and Confidence Intervals','interpreter','latex')
 % hold on
 % histogram(100*SIC_bias(:,5,:),-50:1:50)
 % histogram(100*SIC_bias(:,10,:),-50:1:50)
-% grid on; box on; 
+% grid on; box on;
 % xlim([-20 20])
 % xline([-2.5 2.5],'linewidth',2)
 % legend('Crossing 0','Crossing 5','Crossing 10');
 
-%% 
-subplot(313)
+%%
+subplot(414)
 r = 100*sum(abs(SIC_bias) < 0.025,[1 3]) / numel(SIC_bias(:,1,:));
 s = 100*sum(abs(SIC_bias) < 0.05,[1 3]) / numel(SIC_bias(:,1,:));
 
-plot(1:n_crossings,r,'k')
-hold on; 
+plot(1:perm_length,r,'k')
+hold on;
 
-plot(1:n_crossings,s,'r')
-grid on; box on; 
-xlim([1 n_crossings]); 
+plot(1:perm_length,s,'r')
+grid on; box on;
+xlim([1 perm_length]);
 ylim([0 100])
-legend('Bias < 2.5\%','Bias < 5\%')
+legend('Bias < 2.5%','Bias < 5%')
 
 allAxesInFigure = findall(gcf,'type','axes');
 letter = {'(a)','(b)','(c)','(d)','(e)','(f)','(g)','(e)','(c)'};
 
 for i = 1:length(allAxesInFigure)
-    
- posy = get(allAxesInFigure(i),'position');
+
+    posy = get(allAxesInFigure(i),'position');
 
     set(allAxesInFigure(i),'fontname','times','fontsize',8,'xminortick','on','yminortick','on')
-    
+
     annotation('textbox',[posy(1) - .025 posy(2)+posy(4) + .035 .025 .025], ...
         'String',letter{i},'LineStyle','none','FontName','Helvetica', ...
         'FontSize',8,'Tag','legtag');
---
+++ plotting_driver.m
@@ -0,0 +1,56 @@
+%% Overall code driver for instructions
+% Clear all variables and close all figures
+clear
+close all
+
+% Define the folder paths relative to the current directory
+% This points to the IS2-Emulator repository. 
+
+Output_folder = '/Users/chorvat/Code/IS2-Emulator/Output'; % Adjust based on your local setup
+Plotting_folder = pwd; 
+Script_folder = [Plotting_folder '/Scripts']; 
+
+% For output
+
+% Figure_folder = '~/Library/CloudStorage/Dropbox-Brown/Apps/Overleaf/IS2-Concentration-Part-2/Figures';
+Figure_folder = '/Users/chorvat/Dropbox (Brown)/Apps/Overleaf/IS2-Concentration-Part-2/Figures';
+
+load([Output_folder '/Orientation_Histograms.mat']);
+load([Output_folder '/Emulator_Data.mat']);
+load([Output_folder '/Image_Metadata.mat']);
+
+% Now do some plotting
+addpath(Plotting_folder)
+addpath(Script_folder)
+
+%% Figures related to emulation - should be self-contained. 
+% Orbital orientation figure
+addpath([Plotting_folder '/Emulation-Figures'])
+
+plot_orbital_data(Figure_folder,orientation_hist,lat_disc,orient_disc);
+
+plot_single_image(Figure_folder,image_location,image_done,true_SIC,length_ice_measured,length_measured,sample_orients,sample_points);
+
+plot_emulation_uncertainty(Figure_folder,true_SIC,length_ice_measured,length_measured)
+
+%% Figures related to sea ice concentration comparison with global products
+
+clearvars -except *_folder
+addpath('Product-Figures');
+
+PM_data_folder = '/Users/chorvat/Dropbox (Brown)/Research Projects/Active/Data/SIC-Data';
+IS2_data_string = '/Users/chorvat/Code/IS2-Gridded-Products/Output/IS2_Data_25km_NH_v6.h5';
+
+%%
+
+
+global_product_preamble; 
+
+
+% Here look at global coverage of the IS2 product
+addpath([Plotting_folder '/LIF-Coverage'])
+plot_LIF_coverage; 
+
+
+%% Here look at biases from the IS2 product and other PM data
+
